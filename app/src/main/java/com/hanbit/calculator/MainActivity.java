68e2c37f63c2dffd58d8b631271a8af038b47854
veryberryvery22 <veryberryvery22@gmail.com>
2016-12-10 18:11:49 +0900

    final commit

--- app/src/main/java/com/hanbit/calculator/MainActivity.java
+++ app/src/main/java/com/hanbit/calculator/MainActivity.java
@@ -7,18 +7,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.hanbit.calculator.domain.CalcDTO;
+import com.hanbit.calculator.service.CalcService;
+
+import static com.hanbit.calculator.R.id.etNum3;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText etNum1,etNum3;
+    EditText etNum1,etNum2;
     Button btPlus,btMinus, btMulti, btDivide ,btEqual;
     TextView tvResult;
     int result;
+    CalcService service;
+    CalcDTO cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etNum1 =(EditText) findViewById(R.id.etNum1);
-        etNum3 =(EditText) findViewById(R.id.etNum3);
+        etNum2 =(EditText) findViewById(R.id.etNum2);
 
         btPlus= (Button) findViewById(R.id.btPlus);
         btMinus= (Button) findViewById(R.id.btMinus);
@@ -32,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btMulti.setOnClickListener(this);
         btDivide.setOnClickListener(this);
         btEqual.setOnClickListener(this);
+        service = new CalServiceImpl();
+        cal = new CalcDTO();
 
 
         }
@@ -42,21 +51,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int num1 = Integer.parseInt(etNum1.getText().toString());
-        int num2 = Integer.parseInt(etNum3.getText().toString());
+        int num2 = Integer.parseInt(etNum2.getText().toString());
+        cal.setNum1(num1);
+        cal.getNum2(num2);
+
 
 
         switch(v.getId()){
             case R.id.btPlus:
-                result = num1 + num2;
+                result = service.plus(cal);
                 break;
             case R.id.btMinus:
-                result=num1-num2;
+                result = service.minus(cal);
                 break;
             case R.id.btMulti:
-                result=num1*num2;
+                result = service.multi(cal);
                 break;
             case R.id.btDivide:
-                result=num1 / num2;
+                result = service.divide(cal);
                 break;
 
             case R.id.btEqual:
---
+++ app/src/main/java/com/hanbit/calculator/service/CelcServiceImpl.java
@@ -0,0 +1,32 @@
+package com.hanbit.calculator.service;
+
+import com.hanbit.calculator.domain.CalcDTO;
+
+/**
+ * Created by hb2018 on 2016-12-10.
+ */
+
+public class CelcServiceImpl implements CalcService{
+    @Override
+    public int plus(CalcDTO param) {
+        return param.getNum1() + param.getNum2();
+    }
+
+    @Override
+    public int minus(CalcDTO param) {
+        return param.getNum1() - param.getNum2();
+
+    }
+
+    @Override
+    public int multi(CalcDTO param) {
+        return param.getNum1() * param.getNum2();
+
+    }
+
+    @Override
+    public int divide(CalcDTO param) {
+        return param.getNum1() / param.getNum2();
+
+    }
+}
